7a447a65e903ad4906018427c99e388446b0accb
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2019-08-22 19:47:40 +0200

    continuous feedback of pressure

--- level03/code/sketch_02_read_pressure_neopixel/sketch_02_read_pressure_neopixel.ino
+++ level03/code/sketch_02_read_pressure_neopixel/sketch_02_read_pressure_neopixel.ino
@@ -13,6 +13,35 @@ int Pressure_Sensor = A0;
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel myNeo_neopixels = Adafruit_NeoPixel(2, D3, NEO_RGBW + NEO_KHZ800);
 
+int ard_effect0_status = -1;
+unsigned long ard_effect0_start, ard_effect0_time;
+#define EFFECT0_PERIOD 1000
+
+// Deze functie beschrijven...
+void WriteSerial() {
+  //Variables of this effect are reffered to with ard_effect0
+  boolean restart = false;
+  ard_effect0_time = millis() - ard_effect0_start;
+  if (ard_effect0_time > EFFECT0_PERIOD) {
+    //end effect, make sure it restarts
+    if (ard_effect0_status > -1) {
+    }
+    restart = true;
+    ard_effect0_status = -1;
+    ard_effect0_start = ard_effect0_start + ard_effect0_time;
+    ard_effect0_time = 0;
+  }
+  if (not restart && ard_effect0_status == -1) {
+    ard_effect0_status = 0;
+    ard_effect0_start = ard_effect0_start + ard_effect0_time;
+    ard_effect0_time = 0;
+  Serial.print("Measured Pressure Val [0,1023] = ");
+  Serial.println(PressureADC);
+  }
+}
+
+
+
 void setup() {
   pinMode(Dig_Pressure_LOW, OUTPUT);
   pinMode(Dig_Pressure_HIGH, OUTPUT);
@@ -20,11 +49,14 @@ void setup() {
   myNeo_neopixels.show();
   pinMode(Pressure_Sensor, INPUT);
   Serial.begin(57600);
+  ard_effect0_status = -1;
+  ard_effect0_start = millis();
+
 
   PressureADC = 0;
-  lowPressure = 150;
-  mediumPressure = 400;
-  highPressure = 650;
+  lowPressure = 610;
+  mediumPressure = 800;
+  highPressure = 1000;
   digitalWrite(Dig_Pressure_LOW, LOW);
   digitalWrite(Dig_Pressure_HIGH, HIGH);
 
@@ -32,8 +64,6 @@ void setup() {
 
 void loop() {
   PressureADC = analogRead(Pressure_Sensor);
-  Serial.print("Measured Pressure Val [0,1023] = ");
-  Serial.println(PressureADC);
   if (PressureADC < lowPressure) {
     PressureADC = lowPressure;
   } else if (PressureADC > highPressure) {
@@ -52,8 +82,6 @@ void loop() {
       myNeo_neopixels.show();
     }
   }
-  delay(1000);
-
-  map(0, 0, 1024, 0, 100);
+  WriteSerial();
 
 }
