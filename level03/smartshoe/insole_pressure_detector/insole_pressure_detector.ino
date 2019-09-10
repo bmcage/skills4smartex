f086ca436997f03bc0ed12155105c2884c1fe27d
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2019-09-10 19:23:33 +0200

    Extend with distance sensor, write out time

--- level03/smartshoe/insole_pressure_detector/insole_pressure_detector.ino
+++ level03/smartshoe/insole_pressure_detector/insole_pressure_detector.ino
@@ -14,10 +14,61 @@ const int Pressure_Sensor = A0;
 // Every xx ms write out to serial measurement
 const int WRITE2SERIAL_MS = 10;
 
+#define SERIALTESTOUTPUT true     // show test output on serial monitor at 115200 baud
+#define SERIAL_SPEED     57600    // set speed for serial connection
+/** 
+ *  BEGIN WIFI CONFIGURATION
+ */
+
+// replace this with your wifi credentials
+const bool WPA2_ENTERPRISE_CONNECTION = false;  // false for normal WPA2 connection, true for WPA2 Enterprise
+static const char* username = "xxxxxxxx"; // Username for WPA Enterprise authentification
+
+static const char* ssid = "XXX";
+static const char* password = "XXX";
+
+bool use_static_IP = false;         //use a static IP address
+uint8_t static_IP[4] = {192, 168, 1, 42}; 
+uint8_t static_gateway_IP[4] = {192, 168, 1, 1};// if you want to use static IP make sure you know what the gateway IP is, here 192.168.1.1
+
+//NTP settings
+//#define NTP_ADDRESS  "raspberrypi.local"  // change this to whatever pool is closest (see ntp.org)
+//#define NTP_ADDRESS  "192.168.1.15"       // eth0 address of the raspberry pi Ingegno Maker Space
+#define NTP_ADDRESS "pool.ntp.org"          // a pool on the internet
+
+//dummy matt server/broker needed for wifilib.h
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
+//const char* mqtt_server = "raspberrypi.local";
+const char* mqtt_server = "192.168.1.24";  //eth0 address of the raspberry pi - Ingegno
+
+/** 
+ *  BEGIN VL53L0X DIST SENSOR CONFIGURATION
+ */
+ 
+// install https://github.com/adafruit/Adafruit_VL53L0X
+// via manage libraries in Arduino IDE
+
+// true or false to indicate if dist sensor is present
+const bool VL53L0X_CONNECTED = true;
+const int VL53L0X_SCL_PIN = D1;
+const int VL53L0X_SDA_PIN = D2;
+
 /** 
  *  END CONFIGURATION
  */
- 
+
+
+//wifi and timing lib
+#include "wifilib.h"
+
+WiFiClient client;
+
+//distance sensor
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X dist_sens = Adafruit_VL53L0X();
+VL53L0X_RangingMeasurementData_t dist_measure;
+
 int PressureADC;
 int i;
 
@@ -43,36 +94,79 @@ void WriteSerial() {
     ard_effect0_status = 0;
     ard_effect0_start = ard_effect0_start + ard_effect0_time;
     ard_effect0_time = 0;
-  Serial.print(millis()); Serial.print(", ");
-  Serial.print(PressureADC); Serial.println(", ");
+    Serial.print(millis()); Serial.print(F(", "));
+    Serial.print(PressureADC); Serial.print(F(", "));
+    Serial.print(timeClient.getFullFormattedTime());
+    if (VL53L0X_CONNECTED) {
+      Serial.print(F(", "));
+      if (dist_measure.RangeStatus != 4) {
+        Serial.print(dist_measure.RangeMilliMeter);
+      } else {
+        Serial.print(-1);
+      }
+    }
+    Serial.println(F(", "));
   }
 }
 
 void WriteHeaderSerial(){
   // csv output on serial
-  Serial.print("time"); Serial.print(", ");
-  Serial.print("Measured Pressure Val [0,1023]"); Serial.println(", ");
+  Serial.print(F("time, Measured Pressure Val [0,1023], NTP Time"));
+  if (VL53L0X_CONNECTED) {
+    Serial.print(F(", "));
+    Serial.print(F("Distance (mm)"));
+  }
+  Serial.println(F(", "));
 }
 
 void setup() {
   pinMode(Dig_Pressure_LOW, OUTPUT);
   pinMode(Dig_Pressure_HIGH, OUTPUT);
   pinMode(Pressure_Sensor, INPUT);
-  Serial.begin(57600);
+  Serial.begin(SERIAL_SPEED);
+  // wait until serial port opens for native USB devices
+  //while (! Serial) {
+  //  delay(1);
+  //}
+  
   ard_effect0_status = -1;
   ard_effect0_start = millis();
-
 
   PressureADC = 0;
   digitalWrite(Dig_Pressure_LOW, LOW);
   digitalWrite(Dig_Pressure_HIGH, HIGH);
 
+  //setup distance sensor
+  if (VL53L0X_CONNECTED) {
+    Wire.begin(VL53L0X_SDA_PIN, VL53L0X_SCL_PIN);
+    Serial.println(F("Adafruit VL53L0X test"));
+    if (!dist_sens.begin()) {
+      Serial.println(F("Failed to boot VL53L0X"));
+      while(1);
+    }
+  }
+  //connect to wifi
+  while (WiFi.status() != WL_CONNECTED) {
+   setupWiFi(true); // Connect to local Wifi
+  }
+  ip = WiFi.localIP();
+  obtainedwifi = true;
+
+  // connect to NTP 
+  timeClient.begin();   // Start the NTP UDP client
+  delay(1000);
+  obtainDateTime();
+  NTPstartTijd = millis();
+  currentTime = NTPstartTijd;
+  
   WriteHeaderSerial();
 }
 
 void loop() {
   PressureADC = analogRead(Pressure_Sensor);
+  if (VL53L0X_CONNECTED) {
+    dist_sens.rangingTest(&dist_measure, false); // pass in 'true' to get debug data printout!
+  }
   
   WriteSerial();
-
 }
